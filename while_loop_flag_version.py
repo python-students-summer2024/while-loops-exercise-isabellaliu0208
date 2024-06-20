a945f8a9f3e3c3a5c832155e6b83972459c53178
isabellaliu0208 <yl11064@nyu.edu>
2024-06-20 16:18:21 +0800

    finished！

---
+++ while_loop_flag_version.py
@@ -0,0 +1,24 @@
+def get_starting_number():
+    while True:
+        num = input("How many bottles of beer on the wall? ")
+        if num.isdigit():
+            num = int(num)
+            if num >= 1:
+                return num
+            else:
+                print("Please enter a number 1 or greater.")
+        else:
+            print("Please enter a valid integer.")
+
+def sing(starting_bottles):
+    bottles = starting_bottles
+    keep_singing = True
+    while keep_singing:
+        if bottles > 1:
+            print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.")
+            print(f"Take one down, pass it around, {bottles-1} {'bottles' if bottles-1 > 1 else 'bottle'} of beer on the wall.\n")
+            bottles -= 1
+        elif bottles == 1:
+            print(f"1 bottle of beer on the wall, 1 bottle of beer.")
+            print(f"Take it down, pass it around, no more bottles of beer on the wall!\n")
+            keep_singing = False
